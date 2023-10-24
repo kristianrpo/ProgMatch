9098098572ed005423227c5e17a3598a2de67b38
kristianrpo <krestrepoo@eafit.edu.co>
2023-10-23 21:29:25 -0500

    explanation and recommendations for coding

--- components/institution/urls.py
+++ components/institution/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from .views import ModelView
 app_name = "institutionApp"
 urlpatterns = [
+    path('',ModelView.as_view())
 ]
--- components/institution/views.py
+++ components/institution/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class ModelView(TemplateView):
+    template_name = "institution/institution.html"
 
 # Create your views here.
