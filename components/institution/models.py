a395cd50c7f29138ceee8c447c4772e6d5d979ab
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2023-10-30 10:11:25 -0500

    models

--- components/institution/models.py
+++ components/institution/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class institution(models.Model):
+    idInstitution = models.AutoField(primary_key=True)
+    institutionCode = models.CharField(max_length=45)
+    name = models.CharField(max_length=45)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+    def __str__(self):
+        return f"Institution {self.institution.name}"
--- components/student/models.py
+++ components/student/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+
+
 # Create your models here.
+class student(models.Model):
+    idStudent = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=45)
+    age = models.IntegerField()
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=45)
+    def __str__(self):
+        return f"Name {self.student.name}"
+    
+
+class interest(models.Model):
+    idInterest = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=45)
+    idStudent = models.ForeignKey(student, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Interest {self.interest.name}"
+    
+class skillStudent(models.Model):
+    idSkillStudent = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=45)
+    idStudent = models.ForeignKey(student, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Skill {self.skillStudent.name}"
+    
+class petition(models.Model):
+    idPetition = models.AutoField(primary_key=True)
+    level = models.IntegerField()
+    topic = models.CharField(max_length=45)
+    description = models.CharField(max_length=200)
+    goal = models.CharField(max_length=200)
+    idStudent = models.ForeignKey(student, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"Petition for {self.petition.description}"
+    
+
+
