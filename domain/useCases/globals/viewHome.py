83934f63cdae8189c27ed5e8ab777a3589968fd5
kristianrpo <krestrepoo@eafit.edu.co>
2023-11-01 17:37:05 -0500

    docstring documentation 'viewHome'

--- domain/useCases/globals/viewHome.py
+++ domain/useCases/globals/viewHome.py
@@ -1,4 +1,14 @@
 from django.views.generic import TemplateView
 
 class viewHome(TemplateView):
+    """
+    Generic Django class that allows you to connect to a template and view its contents.
+
+    ...
+    Attributes
+    ----------
+    template_name : str
+        path where the template to be displayed is located.
+    """
+    
     template_name = "home/home.html"
