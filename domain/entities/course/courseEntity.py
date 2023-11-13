993868fa4e028480dac163359907adfcf6aee56e
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2023-11-13 11:56:40 -0500

    courses

--- domain/entities/course/courseEntity.py
+++ domain/entities/course/courseEntity.py
@@ -2,7 +2,13 @@ from django.db import models
 from domain.entities.institution.institutionEntity import institution
 from domain.entities.learningPath.learningPathEntity import learningPath
 
-class course(models.Model):
+class Course(models.Model):
+    DIFFICULTY_CHOICES = [
+        ('facil', 'Fácil'),
+        ('intermedio', 'Intermedio'),
+        ('dificil', 'Difícil'),
+    ]
+
     idCourse = models.AutoField(primary_key=True)
     courseCode = models.CharField(max_length=45)
     name = models.CharField(max_length=100)
@@ -10,12 +16,11 @@ class course(models.Model):
     price = models.FloatField()
     modality = models.CharField(max_length=45)
     content = models.CharField(max_length=200, null=True)
-    description = models.CharField(max_length=200, null = True)
+    description = models.CharField(max_length=200, null=True)
     idInstitution = models.ForeignKey(institution, on_delete=models.CASCADE)
     idLearningPath = models.ForeignKey(learningPath, on_delete=models.CASCADE)
     link = models.CharField(max_length=200)
-    class Meta:
-        app_label = "course"
+    difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
 
     def __str__(self):
-        return f"Course {self.name}"
+        return self.name
