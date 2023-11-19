afc7913ef12a591bfe0408c2594baf4c3860ede0
kristianrpo <krestrepoo@eafit.edu.co>
2023-11-19 13:47:11 -0500

    home and front request student

--- components/learningPath/urls.py
+++ components/learningPath/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from domain.useCases.learningPath.requestStudent import requestStudent
 app_name = "learningPathApp"
 urlpatterns = [
+    path('requestStudent/', requestStudent.as_view(), name = "requestStudent"),
 ]
--- components/student/urls.py
+++ components/student/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from domain.useCases.student.viewInfoStudent import viewInfoStudent
 from domain.useCases.student.updateInfoStudent import updateInfoStudent
 from domain.useCases.student.deleteStudent import deleteStudent
+from domain.useCases.student.viewHomeStudent import viewHomeStudent
 app_name = "studentApp"
 urlpatterns = [
     path('viewInfoStudent/<pk>',viewInfoStudent.as_view(), name = "viewInfoStudent"),
     path('updateInfoStudent/<pk>',updateInfoStudent.as_view(), name = "updateInfoStudent"),
     path('deleteStudent/<pk>',deleteStudent.as_view(), name = "deleteStudent"),
+    path('viewHomeStudent/', viewHomeStudent.as_view(),name = "viewHomeStudent")
 ]
