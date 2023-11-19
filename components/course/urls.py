79841f5c39a889e961c3d2e069e80ff7e879d732
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2023-11-18 21:19:37 -0500

    n

--- components/course/urls.py
+++ components/course/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from domain.useCases.course.addCourses import populate_database
 app_name = "courseApp"
 urlpatterns = [
+    path('populate-database/', populate_database, name='populate_database'),
 ]
