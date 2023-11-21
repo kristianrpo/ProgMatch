daccb573a38869f60698b7583b8b501f451c2f1f
kristianrpo <krestrepoo@eafit.edu.co>
2023-11-21 14:31:58 -0500

    documentation student, learningpath, and all entities

--- domain/entities/learningPath/tagsEntity.py
+++ domain/entities/learningPath/tagsEntity.py
@@ -1,6 +1,30 @@
 from django.db import models
 
 class tags(models.Model):
+    """
+    Django model representing a tag associated with a learning path.
+
+    This model defines the attributes and behavior of a tag, including its unique identifier
+    and name.
+
+    Attributes
+    ----------
+        - idTags: AutoField (primary_key)
+            An automatically incrementing integer field serving as the primary key.
+        - name: str
+            A character field representing the name of the tag.
+
+    Meta
+    ----
+        - app_label: str
+            The label for the learning path app.
+
+    Methods
+    -------
+        - __str__():
+            Returns a string representation of the tag instance, displaying the tag name.
+
+    """
     idTags = models.AutoField(primary_key=True)
     name = models.CharField(max_length=45)
 
--- domain/entities/student/skillStudentEntity.py
+++ domain/entities/student/skillStudentEntity.py
@@ -2,6 +2,30 @@ from django.db import models
 
 
 class skillStudent(models.Model):
+    """
+    Django model representing a skill associated with a student.
+
+    This model defines the attributes and behavior of a skill, including its unique identifier
+    and name.
+
+    Attributes
+    ----------
+        - idSkillStudent: AutoField (primary_key)
+            An automatically incrementing integer field serving as the primary key.
+        - name: str
+            A character field representing the name of the skill.
+
+    Meta
+    ----
+        - app_label: str
+            The label for the student app.
+
+    Methods
+    -------
+        - __str__():
+            Returns a string representation of the skill instance, displaying the skill name.
+
+    """
     idSkillStudent = models.AutoField(primary_key=True)
     name = models.CharField(max_length=45)
     
