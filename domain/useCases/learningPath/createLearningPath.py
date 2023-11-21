041e5220abf50c68134abeb592eed66622191f1f
Luisa María Álvarez García <90728689+lumar2021@users.noreply.github.com>
2023-11-21 11:12:00 -0500

    process description

--- domain/useCases/learningPath/createLearningPath.py
+++ domain/useCases/learningPath/createLearningPath.py
@@ -1,27 +1,55 @@
 import openai
 import ast
+import spacy
 
 openai.api_key = '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'
 
 timeBetweenRequests = 2
 
-def getChatGptResponse(userDescription, level, courseSet):
-    course_descriptions = [course.description for course in courseSet]
 
+def extract_main_topic(user_input):
+    prompt = f"User: {user_input}\nWhat is the main topic or concept in this sentence in a noun, in a single word?"
+
+    try:
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.5,
+            max_tokens=100
+        )
+
+        if response and response['choices'] and response['choices'][0]['text']:
+            return response['choices'][0]['text'].strip()
+        else:
+            return "Could not get a response at this time."
+
+    except openai.error.OpenAIError as e:
+        print(f"Error calling the OpenAI API: {str(e)}")
+        return "Could not get a response at this time."
+
+
+
+
+def getChatGptResponse(user_description,level, course_set):
+    course_descriptions = [course.description for course in course_set]
+    user_description = extract_main_topic(user_description)
+    print(1111111,user_description)
     prompt = (
-        f"User: I want to learn about {userDescription}. "
-        f"I have this set {course_descriptions}, "
-        f"tell me the courses of the set that the general topic of them are related to what I want to learn, that have similar context,just give me the descripctions in a list separated by enter. With no text before, but if there are not related discart them."
+        f"User: I want to learn about {user_description}. "
+        f"I have this set of courses: {(course_descriptions)}. "
+        f"Which courses from the set are topic related to what I want to learn? "
+        f"Provide the course descriptions in a list separated by enter. "
+        f"If there are no related courses, indicate that."
     )
 
     try:
         response = openai.Completion.create(
-            model="text-davinci-002",
+            model="text-davinci-003",
             prompt=prompt,
-            temperature=0.7,
+            temperature=0.5,  
             max_tokens=150
         )
-
+        print(response)
         if response and response['choices'] and response['choices'][0]['text']:
             return response['choices'][0]['text'].strip()
         else:
@@ -33,16 +61,11 @@ def getChatGptResponse(userDescription, level, courseSet):
         return "Could not get a response at this time."
 
 
-responseCache = {}
+
 
 def getChatGptResponseCached(userDescription, level, courseSet):
-    if (userDescription, level) in responseCache:
-        return responseCache[(userDescription, level)]
 
     chatGptResponse = getChatGptResponse(userDescription, level, courseSet)
-
-    responseCache[(userDescription, level)] = chatGptResponse
-
     return chatGptResponse
 
 def filterCourses(courseSet, level, userDescription):
